ea0d7438f600c7d09b6f53feb3a2dfbcc7cb6ac3
zmpl <id6892@alunos.uminho.pt>
2021-12-09 18:33:36 +0000

    ADD: Class diagram example

---
+++ code/src/new_module.h
@@ -0,0 +1,17 @@
+/**
+ * @file new_module.h
+ * @author Jose Pires
+ * @date 2021-12-09
+ *
+ * @brief jakakaalalalaa 
+ *
+ * aaaaaçl aaA
+ */
+
+/**
+ * @brief hakaka
+ * @param ssfsdf
+ * @return dsdfsdf
+ *
+ * dgfsdgdg
+ */
